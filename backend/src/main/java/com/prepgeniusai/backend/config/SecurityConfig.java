1eb4c6e0e9062aa020ef8b8335e0241500c44414
NareshGupta0912 <nareshgupta0912@gmail.com>
2026-09-20 16:45:51 +0530

    update cors

--- backend/src/main/java/com/prepgeniusai/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/prepgeniusai/backend/config/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig {
                 new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of("https://prepgeniusai-app.vercel.app")
         );
 
         configuration.setAllowedMethods(
